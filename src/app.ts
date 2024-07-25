b3b11bf793d6f5ffb33fc85f6a5edabbaf09363f
Sam Walker <samw@kainos.com>
2024-07-25 16:02:14 -0400

    moved views into src directory

--- src/app.ts
+++ src/app.ts
@@ -3,33 +3,31 @@ import nunjucks from "nunjucks";
 import bodyParser from "body-parser";
 import session from "express-session";
 
-import { getAllDatabases } from "./controllers/TestController";
+import {getAllDatabases} from "./controllers/TestController";
+import path from "node:path";
 
 const app = express();
 
-nunjucks.configure('views', {
+nunjucks.configure(path.join(__dirname, 'views'), {
     autoescape: true,
     express: app
 });
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }))
 
 
-var path = require('path');
-app.use('/static', express.static(path.join(__dirname, 'public')));
-//app.use(express.static('views'));
-//app.use(express.static('public'));
-
-app.use(session({ secret: 'SUPER_SECRET', cookie: { maxAge: 28800000 }}));
+app.use(session({secret: 'SUPER_SECRET', cookie: {maxAge: 28800000}}));
 
 declare module "express-session" {
-  interface SessionData {
-    token: string;
-  }
+    interface SessionData {
+        token: string;
+    }
 }
+
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
@@ -38,5 +36,5 @@ app.listen(3000, () => {
 app.get('/', getAllDatabases);
 
 app.get('/index', async (req: express.Request, res: express.Response) => {
-  res.render(path.join(__dirname, '../views', 'index.html'));
+    res.render('index.html');
 })
