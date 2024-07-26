bc24d6c8581356e90aedc61d74cd179388840912
erinmcj <erinmcjannet@gmail.com>
2024-07-26 10:08:08 -0400

    004 style: added nunjucks template to add Kainos styling to pages (#6)
    * style: added Kainos styling to index.html
    
    * style: added a ath for the index.html to the the app.js
    
    * fix: added a use statement in app.js to render html properly
    
    * fix: changed html to make favicon appear
    
    * refactor: moving images to a public folder and making them static
    
    * moved views into src directory
    
    * move views out of src
    
    * refactor: used nunjuckks to create a template for all html pages
    
    ---------
    
    Co-authored-by: Sam Walker <samw@kainos.com>

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import nunjucks from "nunjucks";
 import bodyParser from "body-parser";
 import session from "express-session";
 
-import { getAllDatabases } from "./controllers/TestController";
+import {getAllDatabases} from "./controllers/TestController";
 
 const app = express();
 
@@ -14,19 +14,26 @@ nunjucks.configure('views', {
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }))
 
-app.use(session({ secret: 'SUPER_SECRET', cookie: { maxAge: 28800000 }}));
+
+app.use(session({secret: 'SUPER_SECRET', cookie: {maxAge: 28800000}}));
 
 declare module "express-session" {
-  interface SessionData {
-    token: string;
-  }
+    interface SessionData {
+        token: string;
+    }
 }
+
+app.use(express.static('views'));
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
 app.get('/', getAllDatabases);
+
+app.get('/index', async (req: express.Request, res: express.Response) => {
+    res.render('index.html');
+})
