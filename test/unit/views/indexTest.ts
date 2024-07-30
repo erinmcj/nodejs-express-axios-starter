ac321f2d2ec9cfcf06d0143c3de22a2c2aa148fb
thomkainos <thomas.phan@kainos.com>
2024-07-30 09:55:06 -0400

    test: creating more unit tests for index.html

--- test/unit/views/indexTest.ts
+++ test/unit/views/indexTest.ts
@@ -14,13 +14,43 @@ describe('index.html', () => {
         const renderedHtml = nunjucks.render(templateFile, context);
 
         const expectedImagePaths = [
+            'images/Green_Circle_Favicon.png',
             'images/Kainos_Logo_Blue_Transparent.png',
             'images/Kainos-Logo_White_Transparent.png',
+            'images/Flux_White_Footer.png'
         ];
 
         expectedImagePaths.forEach((expectedSrc) => {
             expect(renderedHtml).to.include(expectedSrc);
         });
+    });
+
+    it('should use the correct style sheet', () => {
+        const viewsPath = path.resolve(__dirname, '../../../views');
+        nunjucks.configure(viewsPath, { autoescape: true });
+
+        const templateFile = 'index.html';
+        const context = {};
+
+        const renderedHtml = nunjucks.render(templateFile, context);
+
+        const expectedStyleSheet = 'style.css';
+
+        expect(renderedHtml).to.include(expectedStyleSheet);
+    });
+
+    it('should display correct contact information', () => {
+        const viewsPath = path.resolve(__dirname, '../../../views');
+        nunjucks.configure(viewsPath, { autoescape: true });
+
+        const templateFile = 'index.html';
+        const context = {};
+
+        const renderedHtml = nunjucks.render(templateFile, context);
+
+        const expectedStyleSheet = 'style.css';
+
+        expect(renderedHtml).to.include(expectedStyleSheet);
     });
 });
 
