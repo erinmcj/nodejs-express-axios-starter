b67ca173a9d345428bceb0071b075d22a289c437
thomkainos <thomas.phan@kainos.com>
2024-07-30 10:25:16 -0400

    test: created unit tests for index view

--- test/unit/views/indexTest.ts
+++ test/unit/views/indexTest.ts
@@ -2,15 +2,14 @@ import path from "path";
 import {expect} from 'chai';
 import nunjucks from "nunjucks";
 
-//TODO: what meaningful tests can we write for index.html? Is it even needed?
 describe('index.html', () => {
+    const templateFile = 'index.html';
+    
+    const viewsPath = path.resolve(__dirname, '../../../views');
+    nunjucks.configure(viewsPath, { autoescape: true });
+
     it('should contain the expected image paths', () => {
-        const viewsPath = path.resolve(__dirname, '../../../views');
-        nunjucks.configure(viewsPath, { autoescape: true });
-
-        const templateFile = 'index.html';
         const context = {};
-
         const renderedHtml = nunjucks.render(templateFile, context);
 
         const expectedImagePaths = [
@@ -26,12 +25,7 @@ describe('index.html', () => {
     });
 
     it('should use the correct style sheet', () => {
-        const viewsPath = path.resolve(__dirname, '../../../views');
-        nunjucks.configure(viewsPath, { autoescape: true });
-
-        const templateFile = 'index.html';
         const context = {};
-
         const renderedHtml = nunjucks.render(templateFile, context);
 
         const expectedStyleSheet = 'style.css';
@@ -40,40 +34,11 @@ describe('index.html', () => {
     });
 
     it('should display correct contact information', () => {
-        const viewsPath = path.resolve(__dirname, '../../../views');
-        nunjucks.configure(viewsPath, { autoescape: true });
-
-        const templateFile = 'index.html';
         const context = {};
-
         const renderedHtml = nunjucks.render(templateFile, context);
 
-        const expectedStyleSheet = 'style.css';
+        const expectedContactInfo = 'Contact us: kainos@kainos.com';
 
-        expect(renderedHtml).to.include(expectedStyleSheet);
-    });
-});
-
-
-//TODO: probably needs to be a new file
-//TODO: tests for other scenarios
-//TODO: tests for more than 1 role? Assert how many table rows?
-//TODO: looks like cheeriojs will help us traversing html: https://cheerio.js.org/docs/basics/loading maybe helpful??
-//TODO: is there any work to have these tests run using npm commands?
-describe('list-job-roles.html', () => {
-    const viewsPath = path.resolve(__dirname, '../../../views');
-    nunjucks.configure(viewsPath, { autoescape: true });
-
-    it('should display job roles when roles are provided', () => {
-        const templateFile = 'list-job-roles.html';
-        const context = {
-            roles: [
-                { roleName: 'Software Engineer', location: 'Toronto', capability: 'Development', band: 'B2', closingDate: '2024-08-01', roleStatus: 1 },
-            ]
-        };
-
-        const renderedHtml = nunjucks.render(templateFile, context);
-
-        expect(renderedHtml).to.include('<td>Software Engineer</td>');
+        expect(renderedHtml).to.include(expectedContactInfo);
     });
 });
