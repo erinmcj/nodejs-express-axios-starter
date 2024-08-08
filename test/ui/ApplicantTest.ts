714455851bc6cec599559c4d33b5a48a21b65c6e
erinmcj <erinmcjannet@gmail.com>
2024-08-08 12:19:03 -0400

    test: added a ui test

--- test/ui/ApplicantTest.ts
+++ test/ui/ApplicantTest.ts
@@ -37,8 +37,36 @@ describe('Applicant UI Test', async () => {
         const endPoint2: string = 'job-roles/1';
         await driver.get(baseUrl + endPoint2);
         
-       const name = await driver.findElement(By.id('roleName')).getText();
+        const name = await driver.findElement(By.id('roleName')).getText();
         expect(name).to.equal('Software Engineer');
+
+        await driver.quit();
+
+    });
+
+    it.only('As an applicant, I can click on a job link and be taken to the correct page', async () => {
+        const driver = new Builder().
+        withCapabilities(Capabilities.chrome()).
+        build();
+
+        const originalWindowHandle = await driver.getWindowHandle();
+
+        const endPoint: string = 'job-roles/1';
+        await driver.get(baseUrl + endPoint);
+
+        const expectedUrl = "https://login.microsoftonline.com/";
+
+
+        await driver.findElement(By.linkText("Job Specification")).click();
+
+        const newWindowHandles = await driver.getAllWindowHandles();
+        const newWindowHandle = newWindowHandles.find(handle => handle !== originalWindowHandle);
+        await driver.switchTo().window(newWindowHandle as string);
+
+
+        const actualUrl = await driver.getCurrentUrl();
+        console.log("actual URL: " + actualUrl);
+        expect(actualUrl).to.include(expectedUrl);
 
         await driver.quit();
 
