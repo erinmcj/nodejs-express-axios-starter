bc825cb85ced8c494c99c07b899bddc347db8373
erinmcj <erinmcjannet@gmail.com>
2024-08-09 14:00:08 -0400

    refactor: simplified code

--- .pa11yci.js
+++ .pa11yci.js
@@ -1,10 +1,10 @@
 var URL_ENV = process.env.URL_ENV;
 var URL;
 
-if(URL_ENV == "Local"){
-    URL = "http://localhost:3000";
-} else {
+if(URL_ENV){
     URL = URL_ENV;
+} else {
+    URL = "http://localhost:3000";
 }
 
 module.exports = {
