c844cdde4872b11c6e4774f858c439fc442e4bf0
erinmcj <erinmcjannet@gmail.com>
2024-08-09 09:11:47 -0400

    feat: adding job role detail page (#15)
    * feat: adding job role detail page
    
    * fix: fixed styling on pages
    
    * test: added and fixed some tests
    
    * test: added ui tests
    
    * fix:fixed linting error
    
    * refactor: added some checks in controller
    
    * fix: fixed linter errors
    
    * refactor: fixed code from comments
    
    * fix: fixed linting checks
    
    * test: added tests cases for views
    
    * fix:fixed linting errors
    
    * refactor: changed ui when error message
    
    * test: added a ui test
    
    * fix:removed it.only from test
    
    * fix:fixed integration test

---
+++ src/models/JobRoleDetailResponse.ts
@@ -0,0 +1,12 @@
+export type JobRoleDetailResponse = {
+    roleId: number,
+    roleName: string,
+    location: string,
+    capability: string,
+    band: string,
+    closingDate: string,
+    roleStatus: number,
+    description: string,
+    responsibilities: string,
+    jobLink: string
+}
